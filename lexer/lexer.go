dcc1e96a6e0140e0ac69b5580445b6628cd57bad
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-04-07 14:57:19 +0100

    Add lexer package with lexer.go file

---
+++ lexer/lexer.go
@@ -0,0 +1,67 @@
+package lexer
+
+import (
+	"github.com/babalolajnr/monkey/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+// New creates a new Lexer instance with the given input string.
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// readChar reads the next character from the input and updates the lexer's state accordingly.
+// If there are no more characters in the input, it sets the lexer's current character to the ASCII code for "NUL".
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // ASCII code for "NUL"
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// NextToken returns the next token in the input string.
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+
+	}
+
+	l.readChar()
+	return tok
+}
+
+// newToken creates a new token with the given token type and character.
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
